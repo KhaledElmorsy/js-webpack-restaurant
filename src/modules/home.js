4cc1e7dd571842bd4647455944d5e90807da884d
kelmorsy <khaled.mg.a@gmail.com>
2022-06-05 17:37:07 +0200

    Add home tab content

--- src/modules/home.js
+++ src/modules/home.js
@@ -1,4 +1,35 @@
-const test = document.createElement('h2')
-test.innerText = 'This is the home page'
+import makeEl from './elementMaker.js'
+import wrap from './wrap.js'
+import pizza from '../assets/pizza.jpeg'
 
-export default test;
+export default function(){
+const introText = 'We know pizza. You come here, you try it, you think: \n Why haven\'t I had pizza this good yet until now? \ That what you can expect when you try our 100 year old recipe that\'s been passed down and perfected for generations. Once you come here, you won\'t go anywhere else'
+
+
+const headline = makeEl('h3','Pizza you just can\'t forget about')
+const heroPic = makeEl('img','',{src:pizza})
+const intro = makeEl('p',introText)
+const statement = makeEl('div')
+
+const statementContents =  (function() {
+    statement.appendChild(makeEl('p','Each pizza has 3 main ingredients that make it perfect: '))
+    
+    const list = makeEl('ol')
+
+    list.appendChild(makeEl('li','Lots of cheese'))
+    list.appendChild(makeEl('li','Lots of toppings'))
+    list.appendChild(makeEl('li','Lots of love'))
+
+    statement.appendChild(list)
+
+    statement.appendChild(makeEl('p','That\'s why people just can\'t get enough of it'))
+
+})();
+
+return [
+    wrap(headline),
+    heroPic,
+    wrap(intro),
+    wrap(statement)
+]
+}
