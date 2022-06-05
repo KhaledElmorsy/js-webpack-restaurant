5027977ac956055c16727dad9a1a8814e4eca50d
kelmorsy <khaled.mg.a@gmail.com>
2022-06-05 22:17:24 +0200

    Add menu page with resources and stlying

--- src/modules/helpers/wrap.js
+++ src/modules/helpers/wrap.js
@@ -1,15 +1,16 @@
-export default function(el, titleText){
-    const wrapper = document.createElement('div')
-    wrapper.classList.add('wrapper')
+import makeEl from "./elementMaker";
+
+export default function(el, titleText, attr, wrapperClass = true){
+    const wrapper = makeEl('div', null, attr)
+    if (wrapperClass) wrapper.classList.add('wrapper')
 
     if (titleText) {
-        const title = document.createElement('h2')
-        title.innerText = titleText;
+        const title = makeEl('h2',titleText)
         wrapper.appendChild(title)
     }
 
     // Can take arrays of elements
-    if (el[0]) for(let el of a) wrapper.appendChild(el);
+    if (el[0]) for(let a of el) wrapper.appendChild(a);
     else wrapper.appendChild(el);
     
     return wrapper;
--- src/modules/tabs/menu.js
+++ src/modules/tabs/menu.js
@@ -1,4 +1,32 @@
-const test = document.createElement('h2')
-test.innerText = 'This is the menu'
+import marghPic from '../../assets/menu/margherita.jpeg'
+import peppPic from '../../assets/menu/pepperoni.jpeg'
+import mushPic from '../../assets/menu/mushroom.jpeg'
 
-export default test;
+import makeEl from '../helpers/elementMaker.js'
+import wrap from '../helpers/wrap.js'
+import '../../assets/menu/menu.style.css'
+
+export default function(){
+    const margheritaText = 'Delicious, alluring, addictive. The classic margherita is here to make your day. Live, Laugh, Love? More like, One, More, Slice'
+    const pepperoniText = 'Meaty, savory, head-spinning insanity. Our pepperoni pizza is like no other. People just can\'t get enough of it!'
+    const mushroomText = 'Umami. What more can we say other than that this pizza is a flavor explosion that\'ll send you to cloud nine and beyond'
+
+    function menuItem(name,text,image){
+        let itemPic = makeEl('img',null,{src:image, class:'menu-pic'})
+        let itemText = makeEl('p',text)
+        
+        let itemContent = wrap([itemPic,itemText],null,{class:'menu-item-content'},false)
+        
+        return wrap(itemContent, name,{class:'menu-item'})
+    }
+
+    const margherita = menuItem('Margherita', margheritaText, marghPic);
+    const pepperoni = menuItem('Pepperoni', pepperoniText, peppPic);
+    const mushroom = menuItem('Mushroom', mushroomText, mushPic);
+
+    return [
+        margherita,
+        pepperoni,
+        mushroom
+    ]
+}
