763b2888394eb502ca644c31738d140c8e9cf3dc
kelmorsy <khaled.mg.a@gmail.com>
2022-06-06 09:47:50 +0200

    Add About page

--- src/modules/tabs/about.js
+++ src/modules/tabs/about.js
@@ -1,4 +1,27 @@
-const test = document.createElement('h2')
-test.innerText = 'This is the about page'
+import makeEl from '../helpers/elementMaker.js'
+import wrap from '../helpers/wrap.js'
+import teamPicAsset from '../../assets/about/our-team.jpeg'
+import '../../assets/about/about.style.css'
 
-export default test;
+export default function(){
+    const historyText = 'More than a hundred years ago, our family set out to make a pizza so flavorful it would make anyone who tried it forget anything else they\'ve had before. They didn\'t just succeed, they kept raising the bar until their pizza was considered to be on a different level from what you can get anywhere else. <br> <br> We haven\'t slowed down one bit, and you can be sure that the pizza you try here is going to absolutely blow your mind and tastebuds.'
+
+    const contact = (function(){
+        let phone = makeEl('p','<b>Phone</b><br> +1 234 456 7890')
+        let address = makeEl('p','<b>Address</b><br> 1234 North-West Road, City')
+        let email = makeEl('p','<b>Email</b><br> contact@pizzapizzapizaaaa.com')
+        let message = makeEl('p','<i>We\'d love to hear from you! Reach out or drop by anytime!<i>')
+
+        return [phone,address,email,message]
+    })();
+
+
+    const history = makeEl('p',historyText)
+    const teamPic = makeEl('img',null,{src:teamPicAsset})
+    
+    return [
+        teamPic,
+        wrap(history,'Our History'),
+        wrap(contact,'Reach Out!',{class:'contact'})
+    ]
+}
